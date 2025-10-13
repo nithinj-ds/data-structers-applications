f277ba14a18f2e8c0772fcebf41aefe524daa28d
nithinj-ds <nithin200302@gmail.com>
2025-10-13 21:33:12 +0530

    Create gcd_recursion.c

---
+++ gcd_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int gcd(int a,int b)
+{
+    if(b==0)
+        return a;
+    return(b,a%b);
+}
+int main()
+{
+    int a,b;
+    printf("\nRead the values for a & b: ");
+    scanf("%d%d",&a,&b);
+    printf("\nThe GCD of %d and %d is %d",a,b,gcd(a,b));
+    return 0;
+}
